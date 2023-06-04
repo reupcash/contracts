915133889843b6482d7a46f47cc31dc0cf80d0fa
reupcash <123663675+reupcash@users.noreply.github.com>
2023-06-04 09:02:17 -0700

    Updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ import "solidity-coverage"
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
@@ -23,6 +23,9 @@ const config: HardhatUserConfig = {
   networks: {
     mainnet: {
       url: "https://mainnet.infura.io/v3/32c733c3e2664c4a8de6b673f5725b97"
+    },
+    arbitrumOne: {
+      url: "https://arb1.arbitrum.io/rpc"
     }
   },
   etherscan: {
@@ -30,7 +33,8 @@ const config: HardhatUserConfig = {
       mainnet: "TBWRD6BIJ3DSF8F1WD8ESNECCIRXJ5DIRP",
       bsc: "KNEGPMNNZ3NFXTRNKZNCYQ4J38NFP694JK",
       opera: "WMU7KNWJPPSEKA6ZIH4PCHU1SUFZA8PZW7",
-      avalanche: "3K4XJU34XS7YXDEZ5N1STAGQ2FWJDJCKPH"
+      avalanche: "3K4XJU34XS7YXDEZ5N1STAGQ2FWJDJCKPH",
+      arbitrumOne: "IBHI1ZJNQ3K6SWKTMXXHMAKXVEYCTNH81W"
     }
   }
 }
